c2c8851f768a975d0ef1aa567fb4db5b58c5c719
Kamil Buczel <kamil.buczel@goyello.com>
2018-02-13 16:11:56 +0100

    Testing .like operator and multipliers

--- uiTestApp/ViewController.swift
+++ uiTestApp/ViewController.swift
@@ -12,18 +12,23 @@ import EasyPeasy
 class ViewController: UIViewController {
     
     var headerView: UIView!
+    
+    fileprivate lazy var userLoginTextfield: UITextField = {
+        let textField = UITextField(frame: CGRect.zero)
+        return textField
+    }()
 
-    fileprivate lazy var userInfoLabel: UILabel = {
-        let label = UILabel(frame: CGRect.zero)
-        return label
+    fileprivate lazy var userPasswordTextfield: UITextField = {
+        let textField = UITextField(frame: CGRect.zero)
+        return textField
     }()
     
-    
-    fileprivate lazy var layout: UIView = {
-        let view = UIView()
+    fileprivate lazy var headerAndTitleBar: UIView = {
+        let view = UIView(frame: CGRect.zero)
         return view
     }()
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -31,10 +36,10 @@ class ViewController: UIViewController {
         setupHeaderAndTitleLabel()
         self.view.backgroundColor = .white
         
-        self.userInfoLabel.text = "test"
-        self.view.addSubview(self.userInfoLabel)
+        self.userLoginTextfield.text = "test"
+        self.view.addSubview(self.userLoginTextfield)
         
-        self.userInfoLabel <- [
+        self.userLoginTextfield <- [
         Height(20),
         Top(60).to(self.view),
         Left(20).to(self.view),
@@ -50,16 +55,24 @@ class ViewController: UIViewController {
     
     func setupHeaderAndTitleLabel() {
         // Initialize views and add them to the ViewController's view
-        headerView = UIView()
-        headerView.backgroundColor = .red
-        self.view.addSubview(headerView)
+        
+        self.headerAndTitleBar.backgroundColor = .red
+        self.view.addSubview(headerAndTitleBar)
         
         // Set position of views using constraints
-        headerView.translatesAutoresizingMaskIntoConstraints = false
-        headerView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
-        headerView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
-        headerView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 1).isActive = true
-        headerView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.1).isActive = true
+        self.headerAndTitleBar <- [
+        Leading(0).to(self.view),
+        Top(0).to(self.view),
+        Width(1).like(self.view),
+        Height(*0.1).like(self.view)
+
+        ]
+        
+//        headerView.translatesAutoresizingMaskIntoConstraints = false
+//        headerView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+//        headerView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+//        headerView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 1).isActive = true
+//        headerView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.1).isActive = true
     }
 
 
